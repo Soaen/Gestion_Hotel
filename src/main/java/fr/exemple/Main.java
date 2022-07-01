0cca301b8a5865eb7a7a9bdf826452d2e7f6c37f
Sylvain Vansteenkiste <sylvain.vansteenkiste59@gmail.com>
2022-07-01 13:55:05 +0200

    Dependency JDBC Driver

--- src/main/java/fr/exemple/Main.java
+++ src/main/java/fr/exemple/Main.java
@@ -3,8 +3,20 @@ package fr.exemple;
 import fr.exemple.services.ServiceExecutor;
 import fr.exemple.services.ServiceMenu;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost/categories";
+        Properties props = new Properties();
+        props.setProperty("user","postgres");
+        props.setProperty("password","197532486a");
+        props.setProperty("ssl","true");
+        Connection conn = DriverManager.getConnection(url, props);
+
         ServiceExecutor se = new ServiceExecutor();
         ServiceMenu sm = new ServiceMenu();
         se.createCSV();
